48dbe218ce9dff29477566a0fe8804a87daf20ac
LaGrunge <leontodys@gmail.com>
2019-10-29 02:25:38 +0300

    Review notes

--- 3party/cttrie/cttrie.hpp
+++ 3party/cttrie/cttrie.hpp
@@ -3,10 +3,8 @@
 #if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wshorten-64-to-32"
-#pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-parameter"
 #include "3party/cttrie/cttrie.h"
-#pragma clang diagnostic pop
 #pragma clang diagnostic pop
 #elif defined(__GNUC__)
 #pragma GCC diagnostic push
