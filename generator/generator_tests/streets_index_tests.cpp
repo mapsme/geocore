cf83d69f67c08c2020714cf754a69ecd9c8b9327
Anatoly Serdtcev <serdtcev@maps.me>
2019-10-30 19:39:46 +0300

    [generator:tests] Add ways index generation test for coverage reach

---
+++ generator/generator_tests/streets_index_tests.cpp
@@ -0,0 +1,87 @@
+#include "testing/testing.hpp"
+#include "generator/generator_tests/common.hpp"
+
+#include "generator/data_version.hpp"
+#include "generator/geo_objects/geo_objects.hpp"
+#include "generator/locality_sorter.hpp"
+
+#include "indexer/locality_index.hpp"
+#include "indexer/locality_index_builder.hpp"
+
+#include "base/assert.hpp"
+
+using namespace generator_tests;
+using namespace generator;
+using namespace indexer;
+using namespace base;
+using namespace std::literals;
+
+using IndexReader = generator::geo_objects::IndexReader;
+using Objects = std::vector<GeoObjectId>;
+
+GeoObjectsIndex<IndexReader> GenerateStreetsIndex(std::vector<OsmElementData> const & osmElements)
+{
+  ScopedFile const geoObjectsFeatures{
+      "geoObjects"s + DATA_FILE_EXTENSION, ScopedFile::Mode::Create};
+  ScopedFile const streetsFeatures{"streets"s + DATA_FILE_EXTENSION, ScopedFile::Mode::DoNotCreate};
+  WriteFeatures(osmElements, streetsFeatures);
+
+  auto const locDataFile = GetFileName("streets"s + LOC_DATA_FILE_EXTENSION);
+  bool locDataGeneration =
+      feature::GenerateGeoObjectsData(geoObjectsFeatures.GetFullPath(),
+                                      streetsFeatures.GetFullPath(),
+                                      boost::none, locDataFile);
+  CHECK(locDataGeneration, ());
+
+  ScopedFile const streetsIndex{"streets"s + LOC_IDX_FILE_EXTENSION, ScopedFile::Mode::DoNotCreate};
+  auto streetsIndexBuilding =
+      BuildGeoObjectsIndexFromDataFile(locDataFile, streetsIndex.GetFullPath(), {},
+                                       DataVersion::kFileTag);
+  CHECK(streetsIndexBuilding, ());
+
+  return ReadIndex<GeoObjectsIndexBox<IndexReader>, MmapReader>(streetsIndex.GetFullPath());
+}
+
+Objects GetObjectsAtPoint(
+    GeoObjectsIndex<IndexReader> const & index, m2::PointD const & atPoint)
+{
+  auto results = Objects{};
+  index.ForEachAtPoint([&] (auto const osmId) { results.push_back(osmId); }, atPoint);
+  std::sort(results.begin(), results.end());
+  return results;
+}
+
+UNIT_TEST(StreetsIndexTest_HighwayIndex)
+{
+  auto const osmElements = std::vector<OsmElementData>{
+      {1, {{"name", "Arbat Street"}, {"highway", "residential"}}, {{1.001, 2.001}, {1.002, 2.001}},
+       {}},
+      {2, {{"name", "New Arbat Street"}, {"highway", "residential"}},
+       {{1.001, 2.002}, {1.002, 2.001}}, {}}};
+
+  auto const & streetsIndex = GenerateStreetsIndex(osmElements);
+
+  auto const arbatEnd = GetObjectsAtPoint(streetsIndex, {1.001, 2.001});
+  TEST_EQUAL(arbatEnd.size(), 1, ());
+  TEST_EQUAL(arbatEnd, Objects({MakeOsmWay(1)}), ());
+
+  auto const junction = GetObjectsAtPoint(streetsIndex, {1.002, 2.001});
+  TEST_EQUAL(junction.size(), 2, ());
+  TEST_EQUAL(junction, Objects({MakeOsmWay(1), MakeOsmWay(2)}), ());
+}
+
+UNIT_TEST(StreetsIndexTest_SquareIndex)
+{
+  auto const osmElements = std::vector<OsmElementData>{
+      {1, {{"name", "New Square"}, {"place", "square"}}, RectArea{{1.000, 2.000}, {1.002, 2.002}},
+       {}}};
+
+  auto const & streetsIndex = GenerateStreetsIndex(osmElements);
+
+  auto outside = GetObjectsAtPoint(streetsIndex, {1.000 - 0.001, 2.000 + 0.001});
+  TEST(outside.empty(), ());
+
+  auto inside = GetObjectsAtPoint(streetsIndex, {1.000 + 0.001, 2.000 + 0.001});
+  TEST_EQUAL(inside.size(), 1, ());
+  TEST_EQUAL(inside, Objects({MakeOsmRelation(1)}), ());
+}
