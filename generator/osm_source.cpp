4a302bee84e3d38baad857efcd143c820f347815
Anatoly Serdtcev <serdtcev@maps.me>
2019-11-05 11:38:06 +0300

    [generator] Optimize features generation: o5m parallel reading

--- generator/osm_source.cpp
+++ generator/osm_source.cpp
@@ -47,8 +47,7 @@ SourceReader::SourceReader(string const & filename)
 
 SourceReader::SourceReader(std::istream & stream)
   : m_file(unique_ptr<istream, Deleter>(&stream, Deleter(false)))
-{
-}
+{ }
 
 uint64_t SourceReader::Read(char * buffer, uint64_t bufferSize)
 {
